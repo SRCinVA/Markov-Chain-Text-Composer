a335673f041f496dc9d7737ac2da1d21c3d3ebe0
SRCinVA <scitarella@hotmail.com>
2021-08-14 16:15:51 -0400

    Bit more challenging but still within reach

--- graph.py
+++ graph.py
@@ -16,4 +16,29 @@ class Vertex:
     def add_edge(self, vertex):  # for words that are already present in the dictionary
         self-adjacent[vertex] =self.adjacent.get(vertex,0) + 1 # plus 1 to account for the new occurence.
         # if the key exists, we'll pull up the value of that pre-existing vertex.
-        # if it doesn't, then it's just 0
+        # if it doesn't, then it's just 0.
+
+    # Now assemble these vertices into a graph
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}    # this will be an empty dicitonary of vertices
+                                # this way, when we encounter a new word,
+                                # we can look it up and get the vertex from thsi dictionary
+
+    def get_vertex_values(self): # this returns all the possible words in the graph (so far).
+        return set(self.vertices.keys*())
+
+    def add_vertex(self, value): # for whenever we encounter a new word.
+        self.vertices[value] = Vertex(value) 
+        # Above, we create a new Vertex object and "put it in this string-to-vertex mapping"
+        # (not really sure what that means ...)
+
+    def get_vertex(self, value): # for when you have a word and need to find the vertex object it represents
+        # if the value isn't in the graph already, then you need to add it in, shown below:
+        if value not in self.vertices:
+            self.add_vertex(value)
+        #otherwise, you just return what's already there:
+        return self.vertices[value]
+
+
