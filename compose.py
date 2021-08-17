a3c471ce00708b0c6fc6e42b4931ad48b8c17a20
SRCinVA <scitarella@hotmail.com>
2021-08-17 17:57:31 -0400

    new file today

---
+++ compose.py
@@ -0,0 +1,12 @@
+# Goals in this section:
+
+get_words_from_text(text_path):
+    with open(text_path, 'r') as f: # why is Python mad at 'open'?
+        text = f.read()    
+
+def main():
+# 1.) get words from the text
+# 2.) make a graph using those words
+# 3.) get the next word for x number
+# 4.) show these to the user
+pass
