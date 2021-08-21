e23347bfe7208960430009e84d188f675dcea715
SRCinVA <scitarella@hotmail.com>
2021-08-21 17:27:13 -0400

    to 2:48:55

--- compose.py
+++ compose.py
@@ -1,4 +1,5 @@
 import string
+import graph from Graph, Vertex
 
 # Goals in this section:
 
@@ -16,6 +17,18 @@ get_words_from_text(text_path):
         text = text.translate(str.maketrans('','', string.punctuation))
     words = text.split() # 'words' are whatever falls out of the .split() operation.
     return words
+# now we need to make a graph using those words
+def make_graph(words):
+    # we import the graph and the vertices from the graphpy file
+    # (note the importation above)
+    g = Graph
+
+    # then, for each word in Words, we're gong to check to see if it's in the graph
+    # if it's not, we add it.
+    # If there was a previous word (every one after the first, clearly),
+    # then add an edge to the graph if the current word does not exist.
+    # if it *does* exist, then just increment the weight of the existing edge by 1.
+    # set the word to the previous word (so you can move to the next one) and iterate.
 
 def main():
     # 1.) get words from the text
